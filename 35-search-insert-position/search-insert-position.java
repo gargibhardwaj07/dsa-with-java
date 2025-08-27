814e446c8a8d3252b742d675eaf2db72f0e22ba8
Gargi Bhardwaj <150110213+gargibhardwaj07@users.noreply.github.com>
2025-08-27 19:01:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (93.29%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        //in this i am using lower bound approach
+        int n = nums.length;
+        int low = 0; //index 0
+        int high = n-1; //index last
+        int ans = n; //if not find ans ength index like if array of size 10 so ans 10
+        while(low <= high){
+            int mid = (low+high)/2;
+
+            //agr mid bda h target se ex: target = 1 mid h hmara 3 to hum aab lower se mid tk dudhnge kyuki nd me nhi milega or bde elmnt se h aage
+            if(nums[mid] >= target){ 
+                ans = mid; //possibilty only
+                high = mid-1;
+
+            }
+            else{
+                low = mid+1;
+            }
+
+        }
+ return ans;
+        
+    }
+}
