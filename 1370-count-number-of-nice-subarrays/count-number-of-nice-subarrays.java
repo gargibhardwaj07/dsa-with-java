4301cfb5bb47f0e0f892512692b4eed8836bd979
Gargi Bhardwaj <150110213+gargibhardwaj07@users.noreply.github.com>
2025-09-17 20:53:43 +0530

    Time: 10 ms (84.54%) | Memory: 55 MB (70.47%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,13 +1,13 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        //here we want odd sum so we make if its odd no = 1 and even =0; 
-          for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                nums[i] = 0;
-            } else {
-                nums[i] = 1;
-            }
-        }
+        // //here we want odd sum so we make if its odd no = 1 and even =0; 
+        //   for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] % 2 == 0) {
+        //         nums[i] = 0;
+        //     } else {
+        //         nums[i] = 1;
+        //     }
+        // }
 
         return atMost(nums, k) - atMost(nums, k-1);
     }
@@ -25,10 +25,10 @@ class Solution {
         //here we first find sum <= goal
         while(r <n){
            
-            sum += nums[r];
+            sum += nums[r]%2; //means if only odd
 
             while(sum > k){ //jn bhi sum bd gya goal s we shrink left
-            sum -= nums[l];
+            sum -= nums[l]%2;
             l++;
          }
             scount = scount + (r-l+1); // we know length jitni he h subraay
