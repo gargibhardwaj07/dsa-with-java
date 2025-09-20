33a766bcc6763198d0d91ef9bd66e604057f38e1
Gargi Bhardwaj <150110213+gargibhardwaj07@users.noreply.github.com>
2025-09-20 23:14:45 +0530

    Time: 2 ms (84.20%) | Memory: 45.9 MB (43.75%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,13 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-         int goal = nums.length - 1;
-
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (i + nums[i] >= goal) {
-                goal = i;
+        int maxlen =0; //idx 0 pr abhi 0 he h max 
+        for(int i =0;i < nums.length; i++){
+            //agr i bda h mtlb index max len se to mtlb hum nhi phoche last tk
+            if(i > maxlen){
+                return false;
+            }
+            //updat emaxlen
+            maxlen = Math.max(maxlen, nums[i]+i);
+            //agr maxlen ki value bdi hogye to mtlb hum index se jyda phoch gye
+            if(maxlen > nums.length){
+                return true;
             }
         }
-
-        return goal == 0;    
+        return true;
+        
     }
 }
