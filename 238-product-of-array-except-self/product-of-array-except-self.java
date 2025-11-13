ad2327c452d3b3567488d52ae283148d75e59596
Gargi Bhardwaj <150110213+gargibhardwaj07@users.noreply.github.com>
2025-11-13 11:09:00 +0530

    Time: 2 ms (88.95%) | Memory: 72.2 MB (6.71%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int [n];
+      
+ 
+ //step 1 calc prefix like product of all before curr indx
+       ans[0] = 1;
+        for(int i = 1; i<n;i++){
+           ans[i] = ans[i-1] * nums[i-1];
+        }
+
+         int suffix =1;
+        for(int i = n-2; i>=0;i--){
+            suffix *= nums[i+1];
+            ans[i] *= suffix;
+        }
+
+      
+        return ans;
+    }
+}
