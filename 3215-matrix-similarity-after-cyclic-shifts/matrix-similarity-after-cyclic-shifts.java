b838cdd76eaff8b7579446c44bd05eb185397e14
Gargi Bhardwaj <150110213+gargibhardwaj07@users.noreply.github.com>
2026-03-28 00:52:02 +0530

    Time: 1 ms (99.47%) | Memory: 46.8 MB (82.40%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int shift = k % n;
+
+        // If no effective shift, matrix is same
+        if (shift == 0) return true;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i % 2 == 0) {
+                    // even row -> left shift
+                    if (mat[i][j] != mat[i][(j + shift) % n]) {
+                        return false;
+                    }
+                } else {
+                    // odd row -> right shift
+                    if (mat[i][j] != mat[i][(j - shift + n) % n]) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
